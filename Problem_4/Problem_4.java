6602b531e598924e071a4e7320d3e187dd97d99c
Byunggwan Lim <bg_lim@hotmail.com>
2017-01-02 12:58:19 -0800

    Problem_4 Performance improvement. 48ms->2ms, 95.83% improvement.

--- Problem_4/Problem_4.java
+++ Problem_4/Problem_4.java
@@ -1,52 +1,42 @@
-
 public class Problem_4 {
     public static void main(String[] args) {
+        final long startTime = System.currentTimeMillis();
+        
         int result = 0; 
-        for(int num_1 = 100; num_1 < 1000; num_1++) {
-            for(int num_2 = 100; num_2 < 1000; num_2++) {
+        int num_2_bound = 900;
+        for(int num_1 = 999; num_1 > 0; num_1--) {
+            for(int num_2 = num_1; num_2 >= num_2_bound; num_2--) {
                 int temp = num_1 * num_2;
-                boolean is_palindrome = true;
+                
+                if(temp < result)
+                    continue;
+
                 char[] array = Integer.toString(temp).toCharArray();
-
-
-                int length = 0;
-                int length_2 = 0;
-                //even
-                if(array.length % 2 == 0) {
-                    length = array.length/2;
-                    length_2 = array.length - 1;
-                    for(int i = 0; i < length; i++) {
-                        if(array[i] !=array[length_2]) {
-                            is_palindrome = false;
-                            break;
-                        }   
-                        length_2--; 
-                    }
-                    if(is_palindrome == true) { 
-                        if(temp > result)
-                            result = temp;
-                    }
-                }
-
-                //odd
-                else {
-                    length = array.length/2;
-                    length_2 = array.length - 1;
-                    for(int i = 0; i < length; i++) {
-                        if(array[i] != array[length_2]) {
-                            is_palindrome = false;
-                            break;
-                        }   
-                        length_2--; 
-                    }
-                    if(is_palindrome == true) {
-                        if(temp > result)
-                            result = temp;
-                    }
-
+                if(palindrome_checker(array) == true) { 
+                    if(temp > result) 
+                        result = temp;
                 }
             }
         }
         System.out.println(result);
+        
+        final long endTime = System.currentTimeMillis(); 
+        System.out.println("Total execution time: " + (endTime - startTime) + "ms");    
+    } 
+
+    public static boolean palindrome_checker(char[] array) {
+        boolean is_palindrome = true;
+        
+        int length_front = array.length/2;
+        int length_back = array.length - 1;
+        for(int i = 0; i < length_front; i++) {
+            if(array[i] !=array[length_back]) {
+                is_palindrome = false;
+                break;
+            }   
+            length_back--; 
+        }
+
+        return is_palindrome;
     }
 }
